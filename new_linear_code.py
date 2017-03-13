19c0a72f65e2595fb39ed61ce4acaaf25e1ed397
Nick Montoni <nick.montoni@gmail.com>
2017-03-13 16:19:55 -0700

    blah

--- new_linear_code.py
+++ new_linear_code.py
@@ -3,7 +3,7 @@ import math
 import matplotlib.pyplot as plt
 
 '''Begin by choosing a number of particles and a number of rings, defining constants and material properties'''
-numPart = 3
+numPart = 2
 numRings = 1
 mie_omegas = np.loadtxt('mie_omegas_eV.txt')
 c = 3.0e8 # speed of light in m/s
@@ -35,25 +35,27 @@ for rad in radius:
 	Loc = []
 	for part in range(numPart):
 		Loc.append(np.array([dist_to_center*math.cos(theta[part]), dist_to_center*math.sin(theta[part])]))
-	#print Loc
+	print Loc
 	alphasp = (a0**3) * (3/(epsinf+2*epsb)) # polarizability - note: may need factor of 4*pi*eps0
 	
 	# initialize loop over modes, frequencies, etc.
 	# this will have to be adjustable for each specific case
 	H = np.zeros((2*numPart,2*numPart),dtype=complex)
 	eigen = np.zeros(2*numPart)
-	omega_mode = np.real(omega_sp)
+	omega_mode = omega_sp
 	count = 1
 	
 	for mode in range(4):
-		while np.absolute(np.real(omega_mode) - np.real(eigen[2*numPart - (mode+1)])) > 0.00001:
-			print omega_mode
-			print eigen[2*numPart - (mode+1)]
-			print omega_mode - eigen[2*numPart - (mode+1)]
-			count = count +1
-			print rad
-			Q = [[1,0],[0,1],[1,0],[0,1],[1,0],[0,1]] # dipole moments in x- and y-direction
-			omega_mode = np.real(eigen[2*numPart - (mode+1)])
+		while np.absolute(omega_mode - eigen[2*numPart - (mode+1)]) > 0.00001:
+			if count == 1:
+				Q = [[1,0],[0,1],[1,0],[0,1]] # dipole moments in x- and y-direction
+				#count = count + 1
+			else:
+				pass
+				#Q = np.reshape(vec[2*numPart - (mode+1)],(numPart,2))
+				#print Q
+				#raw_input()
+			omega_mode = eigen[2*numPart - (mode+1)]
 			wavenumber = (omega_mode*elec)/(c*hbar*math.sqrt(epsb))
 			alpha = alphasp/(1 - 1j*(2./3.)*(wavenumber**3)*alphasp)
 			mass = 1/(4*math.pi*vacuum_perm)*(elec**2)/(alpha*(omega_sp*elec/hbar)**2)
@@ -66,36 +68,38 @@ for rad in radius:
 					#print r_ij_mag
 					nhat_ij = (Loc[i/2] - Loc[j/2])/r_ij_mag
 					if i == j:
-						H[i,j] = -1
+						H[i,j] = 1
 					elif r_ij_mag < 0.1*nm:
 						H[i,j] = 0
 					else:
 						#modified_distance = r_ij_mag - a0
-					
-						Q_dot_Q = np.dot(Q[i],Q[j])
-						Q_nn_Q = np.dot(Q[i],nhat_ij)*np.dot(nhat_ij,Q[j])
+						if count == 1:
+							Q_dot_Q = np.dot(Q[i],Q[j])
+							Q_nn_Q = np.dot(Q[i],nhat_ij)*np.dot(nhat_ij,Q[j])
 							#count = count + 1
-						
+						else:
+							Q_dot_Q = np.dot(Q[i/2],Q[j/2])
+							Q_nn_Q = np.dot(Q[i/2],nhat_ij)*np.dot(nhat_ij,Q[j/2])
 						# now write field terms
 						near_field = (3*Q_nn_Q - Q_dot_Q)/(r_ij_mag**3)
 						int_field = -(3*Q_nn_Q - Q_dot_Q)/(r_ij_mag**2) * 1j * wavenumber
-						far_field = (Q_dot_Q - Q_nn_Q)/(r_ij_mag) * wavenumber**2
+						far_field = 2*(Q_dot_Q - Q_nn_Q)/(r_ij_mag) * wavenumber**2
 						exponential = np.exp(1j*wavenumber*r_ij_mag)
 						coupling = (near_field + int_field + far_field) * exponential
 						H[i,j] = -(alpha * coupling)
 			zero_block = np.zeros(H.shape,dtype=complex)
 			identity_block = np.identity(H.shape[0],dtype=complex)
-			gamma_block = -identity_block*gamma
-			full_matrix = np.vstack([np.hstack([zero_block, identity_block]), np.hstack([(omega_sp**2)*H, gamma_block])])
-			print (omega_sp**2)*H
-			eigenValues, eigenVectors = np.linalg.eig(full_matrix)
-			print eigenValues
-			raw_input()
+			gamma_block = -1j*identity_block*gamma/(omega_sp)
+			full_matrix = np.vstack([np.hstack([zero_block, identity_block]), np.hstack([-H, gamma_block])])
+			#print full_matrix
+			eigenValues, eigenVectors = np.linalg.eig(H)
+			#print eigenValues
+			#raw_input()
 			idx = eigenValues.argsort()[::-1] # this is the idx that sorts the eigenvalues from largest to smallest
 			eigen = np.sqrt(eigenValues[idx]) * omega_sp
 			vec = eigenVectors[:,idx]
 			#print vec
-		#print eigen[2*numPart - (mode+1)]
+		print eigen[2*numPart - (mode+1)]
 		normal_modes[mode].append(eigen[2*numPart - (mode+1)])
 
 plt.figure()
