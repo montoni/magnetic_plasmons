e94e67c4331899ceaee706b1b970aa0f0b26448f
Nick Montoni <nick.montoni@gmail.com>
2017-06-16 09:27:01 -0700

    ahhaha

--- magnets_bem_plotter.m
+++ magnets_bem_plotter.m
@@ -2,11 +2,11 @@ clear all
 close all
 clc
 
-for num = [1,15,30]
-    A = load(strcat('optical/twomer_optical_nm',num2str(num)));
+for num = 1:10
+    A = load(strcat('optical/twomer_optical_sep',num2str(num)));
     clf
     eV = 1240./(A(:,1))
-    plot(eV,A(:,2)-A(:,3),eV,A(:,4)-A(:,5))
+    plot(eV,A(:,3),eV,A(:,5))
     legend('xy' , 'yz')
     num
     pause
