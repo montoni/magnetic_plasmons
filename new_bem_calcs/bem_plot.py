991dae3dab3f0e3e1d251bbe2e93bfc93a170a09
Nick Montoni <nick.montoni@gmail.com>
2018-01-17 15:05:38 -0800

    deleted obsolete directories and data

--- new_bem_calcs/bem_plot.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-for num in [1,4,7,10]:
-	A = np.loadtxt('nm' + str(num) + '_twomer')
-	plt.figure()
-	plt.plot(A[:,0],A[:,1],A[:,0],A[:,2])
-	plt.show()
